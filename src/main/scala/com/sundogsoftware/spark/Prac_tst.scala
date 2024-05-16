718a08a0f0de72217f921f2b95d5ab0df5ae174d
dileepjr10 <dileepjr10@gmail.com>
2024-05-16 22:02:11 +0530

    newclass

---
+++ src/main/scala/com/sundogsoftware/spark/Prac_tst.scala
@@ -0,0 +1,26 @@
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.DataFrame
+
+
+object Prac_tst extends App{
+
+  import org.apache.spark.sql.SparkSession
+  import org.apache.log4j._
+
+  Logger.getLogger("org").setLevel(Level.ERROR)
+
+  // Create a SparkSession
+  val spark = SparkSession.builder()
+    .appName("DataFrameExample")
+    .master("local[*]")
+    .getOrCreate()
+
+  // Create a DataFrame from a sequence of tuples
+  val data = Seq(("John", 25), ("Alice", 30), ("Bob", 35))
+  val df:DataFrame = spark.createDataFrame(data).toDF("Name", "Age")
+
+  // Show the DataFrame
+  df.show()
+
+
+}
